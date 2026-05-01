4e290d62fb3890a55fc9066d3815b183d6a01473
A-liver <150110395+A-liver@users.noreply.github.com>
2026-05-01 22:46:03 +0800

    Create fetch_cities.php

---
+++ api/fetch_cities.php
@@ -0,0 +1,30 @@
+<?php
+$r=$_REQUEST['r'];
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "edata_db";
+$conn;
+// Create connection
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+ $data = array();
+        $sql = "SELECT  CITIES_MUNICIPALITIES  FROM cities where REGION ='$r' ORDER BY CITIES_MUNICIPALITIES ASC";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+        }
+// Close connection
+$conn->close();
+    
+header('Content-Type: application/json'); // Indicate JSON response
+echo json_encode($data); // Output the data as a JSON array
+?>
